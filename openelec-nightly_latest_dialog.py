aaa65a6434cc04ed22a306f05c7144d33c8e6731
x23piracy <x23piracy@hotmail.com>
2012-04-11 01:24:08 +0200

    changes3

--- openelec-nightly_latest_dialog.py
+++ openelec-nightly_latest_dialog.py
@@ -9,4 +9,4 @@ dialog = xbmcgui.Dialog()
 i = dialog.yesno("An Update is Available", "New Build "+VERSION+" available Install?")
 
 if i == 1:
-    subprocess.call("/storage/script/openelec-nightly_latest.sh -q", shell=True)
+    subprocess.call("/storage/script/openelec-nightly_latest.sh -q -b", shell=True)
